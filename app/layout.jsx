15628175383f8d49ab26a8451c04510f4533cc2b
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2026-05-26 20:24:21 -0300

    Animate hero creative lettering with display fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,15 @@
-import { Manrope, Space_Grotesk } from "next/font/google";
+import {
+  Bungee_Shade,
+  Ewert,
+  Fascinate_Inline,
+  Manrope,
+  Monoton,
+  Pirata_One,
+  Rubik_Glitch,
+  Rubik_Moonrocks,
+  Space_Grotesk,
+  UnifrakturMaguntia,
+} from "next/font/google";
 import "./globals.css";
 
 const manrope = Manrope({
@@ -9,6 +20,54 @@ const manrope = Manrope({
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   variable: "--font-display",
+});
+
+const bungeeShade = Bungee_Shade({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-bungee-shade",
+});
+
+const monoton = Monoton({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-monoton",
+});
+
+const rubikMoonrocks = Rubik_Moonrocks({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-rubik-moonrocks",
+});
+
+const rubikGlitch = Rubik_Glitch({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-rubik-glitch",
+});
+
+const pirataOne = Pirata_One({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-pirata-one",
+});
+
+const ewert = Ewert({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-ewert",
+});
+
+const fascinateInline = Fascinate_Inline({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-fascinate-inline",
+});
+
+const unifraktur = UnifrakturMaguntia({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-unifraktur",
 });
 
 export const metadata = {
@@ -25,7 +84,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={`${manrope.variable} ${spaceGrotesk.variable}`}>
+      <body
+        className={`${manrope.variable} ${spaceGrotesk.variable} ${bungeeShade.variable} ${monoton.variable} ${rubikMoonrocks.variable} ${rubikGlitch.variable} ${pirataOne.variable} ${ewert.variable} ${fascinateInline.variable} ${unifraktur.variable}`}
+      >
         {children}
       </body>
     </html>
